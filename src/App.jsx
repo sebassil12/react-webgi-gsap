cda94f763a16a679d20cb93a73f73b6218aac465
sebassil12 <anddysilva15@gmail.com>
2023-10-15 22:26:20 -0400

    First version, without implement model 3D

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import SoundSection from "./components/SoundSection";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Nav />
+      <Jumbotron />
+      <SoundSection />
+    </div>
+  );
+}
+
+export default App;
