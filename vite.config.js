e3fde5c96aaa6d8810d217f6e84654bd39261aa3
sebassil12 <anddysilva15@gmail.com>
2023-10-26 12:46:52 -0400

    change URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://www.deployprojectiphone.com/",
+  base:"https://www.projectportiphone.com/",
 })
